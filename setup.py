d61ed987eb934eba980c3423226307d8eb1b678a
Jack Scott <jscott@revpointmedia.com>
2013-10-13 11:30:25 -0400

    Initial import from
    https://code.google.com/p/google-goopy/source/detail?r=7a101894eb95d3db9f627f69e25edae0b8502c05

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='goopy',
+      version='0.1',
+      description='Google Python Utilities',
+      license='BSD',
+      author='Google Inc.',
+      author_email='opensource@google.com',
+      url='http://goog-goopy.sourceforge.net',
+      packages=['goopy'],
+     )
